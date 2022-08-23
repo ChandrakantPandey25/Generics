df01a9702ad55a3a727942c2d0de43ac3b654354
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-22 23:57:05 -0700

    Added MaxFString

---
+++ MaxOf3Integer/MaxOf3Integer/MaxString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum
+{
+    class MaxString
+    {
+        public void MString()
+        {
+            Console.WriteLine("Enter the first string");
+
+            string firstString = Console.ReadLine();
+                int firstStrLength=firstString.Length;
+            Console.WriteLine("Enter the second string");
+            string secondString = Console.ReadLine();
+            int secondStrLength = secondString.Length;
+            
+            Console.WriteLine("Enter the third string");
+            string thirdString = Console.ReadLine();
+            int thirdStrLength = thirdString.Length;
+            
+            if (firstStrLength > secondStrLength)
+            {
+                if (firstStrLength > thirdStrLength)
+
+                {
+                    Console.Write("Largest string is \n" + firstString);
+                }
+                else
+                {
+                    Console.Write("Largest string is \n" + thirdString);
+                }
+            }
+
+            else if (secondStrLength > thirdStrLength)
+
+                Console.Write("Largest string is \n" + secondString);
+
+            else
+
+                Console.Write("Largest string is \n" + thirdString);
+
+
+        }
+    }
+}
--- MaxOf3Integer/MaxOf3Integer/Program.cs
+++ MaxOf3Integer/MaxOf3Integer/Program.cs
@@ -9,6 +9,7 @@ namespace Maximum
             Console.WriteLine("Welcome to Generics Program");
             Console.WriteLine("Enter 1 for Maximum of three integers");
             Console.WriteLine("Enter 2 for Maximum of three float numbers");
+            Console.WriteLine("Enter 3 for Maximum of three strings");
 
             switch(Convert.ToInt32(Console.ReadLine()))
             {
@@ -19,6 +20,10 @@ namespace Maximum
                 case 2:
                     MaxFloat largestFNum = new MaxFloat();
                     largestFNum.FloatNum();
+                    break;
+                case 3:
+                    MaxString largestSNum = new MaxString();
+                    largestSNum.MString();
                     break;
                 default:
                     Console.WriteLine("Enter Valid number!!");
